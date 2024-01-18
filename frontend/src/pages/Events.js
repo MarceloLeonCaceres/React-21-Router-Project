10353e7af98f7033c38449e3543dd7c11e8f479b
Marcelo León Cáceres <marcelo.leon.c@gmail.com>
2024-01-18 11:47:18 -0500

    480 Deferring Data Fetching with defer()

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,33 +1,35 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
+import { Suspense } from "react";
 
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  // if(data.isError){
-  //   return <p>{data.message}</p>
-  // }
-  const eventos = data.events;
-
-  return <EventsList events={eventos} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // return { isError: true, message: 'No pudo traer los eventos.'};
-    // throw new Response(JSON.stringify({ message: 'Could not fetch events.'}), {
-    //   status: 500,
-    // });
-    return json(
-      { message: 'Could not fetch events.'}, 
-      { status: 500,}
-    );
+    return json({ message: "Could not fetch events." }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
